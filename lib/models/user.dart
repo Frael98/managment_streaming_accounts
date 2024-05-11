85a61ddea57c0361b75dbd825479bd7a1670f166
Frael98 <frael1984@gmail.com>
2024-05-10 22:45:45 -0500

    fromMap agregado

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -2,7 +2,6 @@
 import 'package:f_managment_stream_accounts/interfaces/entity.dart';
 
 class User extends Entity {
-  
   int? idUser;
   final String name;
   final String lastname;
@@ -11,25 +10,36 @@ class User extends Entity {
   final int? age;
   final String password;
 
-  User({
-    required this.name,
-    required this.lastname,
-    required this.user,
-    required this.email,
-    this.age,
-    required this.password
-  });
+  User(
+      {this.idUser,
+      required this.name,
+      required this.lastname,
+      required this.user,
+      required this.email,
+      this.age,
+      required this.password});
 
   // Método para convertir la instancia de la clase a un mapa
   Map<String, dynamic> toMap() {
+    // alias: toJson
     return {
       'user': user,
       'name': name,
-      'lastname' : lastname,
+      'lastname': lastname,
       'age': age,
       'email': email,
       'password': password
     };
+  }
+
+  factory User.fromMap(Map<String, dynamic> map) {
+    return User(
+        idUser: map['iduser'],
+        name: map['name'],
+        lastname: map['lastname'],
+        user: map['user'],
+        email: map['email'],
+        password: map['password']);
   }
 
   // Implement toString to make it easier to see information about
