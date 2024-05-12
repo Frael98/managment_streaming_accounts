4978f7a8f1356edbee784fb112984ed24ce381d9
Frael98 <frael1984@gmail.com>
2024-05-12 17:23:22 -0500

    agregando campos de auditoria

--- lib/db/database.dart
+++ lib/db/database.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:developer';
 
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
@@ -16,11 +17,38 @@ Future<Database> connectToDb() async {
 }
 
 /// Creacion de tablas
-void createUsetTable()  async {
+void createUsetTable() async {
   //Obtenemos la conexion
   final db = await connectToDb();
 
-  db.execute('CREATE TABLE USER('
-    ' IDUSER INTEGER PRIMARY KEY, NAME TEXT, LASTNAME TEXT, USER TEXT,'
-    ' EMAIL TEXT, PASSWORD TEXT); ');
+  try {
+    db.execute('CREATE TABLE USER('
+        ' IDUSER INTEGER PRIMARY KEY, NAME TEXT, LASTNAME TEXT, USER TEXT,'
+        ' EMAIL TEXT, AGE INT, PASSWORD TEXT, STATE TEXT, CREATED_AT DATETIME, UPDATED_AT DATETIME, DELETED_AT DATETIME); ');
+
+    log('La tabla USER se ha creado correctamente.');
+  } catch (e) {
+    log('Hubo un error al crear la tabla: $e');
+  }
+
+  try {
+    db.execute('CREATE TABLE CLIENT('
+        ' ID_CLIENT INTEGER PRIMARY KEY, NAME_CLIENT TEXT, NUMBER_PHONE TEXT, DIRECTION TEXT,'
+        ' EMAIL TEXT, STATE TEXT, CREATED_AT DATETIME, UPDATED_AT DATETIME, DELETED_AT DATETIME); ');
+
+    log('La tabla CLIENT se ha creado correctamente.');
+  } catch (e) {
+    log('Hubo un error al crear la tabla: $e');
+  }
+
+  try {
+    db.execute('CREATE TABLE ACCOUNT('
+        ' ID_ACCOUNT INTEGER PRIMARY KEY, EMAIL TEXT, PASSWORD TEXT, REGISTER_DATE DATETIME,'
+        ' EXPIRE_DATE DATETIME, PLATFORM TEXT, TYPE_ACCOUNT TEXT, PERFIL_QUANTITY INT'
+        ' , STATE TEXT, CREATED_AT DATETIME, UPDATED_AT DATETIME, DELETED_AT DATETIME);');
+
+    log('La tabla ACCOUNT se ha creado correctamente.');
+  } catch (e) {
+    log('Hubo un error al crear la tabla: $e');
+  }
 }
