0a8049a8e488bb197c6827f0a2f96aa158d61cb4
Frael98 <frael1984@gmail.com>
2024-05-13 21:20:26 -0500

    new view

---
+++ lib/forms/plataforma/platform_list.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PlatformListView extends StatelessWidget {
+  const PlatformListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
