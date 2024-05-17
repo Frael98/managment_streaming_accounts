a5b8afeb4f25f31d59849dd23f4c3cd55c49c3ef
Frael98 <frael1984@gmail.com>
2024-05-16 19:23:40 -0500

    new functions added

--- lib/utils/helpful_functions.dart
+++ lib/utils/helpful_functions.dart
@@ -1,33 +1,86 @@
-  String? messageValidator(String? formText) {
-    if (formText!.isEmpty) return "Campo obligatorio";
+import 'package:flutter/material.dart';
+import 'package:flutter_dialogs/flutter_dialogs.dart';
+import 'package:toast/toast.dart';
 
-    return null;
+/// Verifica si un objeto es nulo
+bool isNull(Object? object){
+  return object != null;
+}
+
+///Validacion de cualquier tipo de campo
+String? messageValidator(String? formText) {
+  if (formText!.isEmpty) return "Campo obligatorio";
+
+  return null;
+}
+
+///Validacion de formato email
+String? validateEmail(String? formEmail) {
+  if (formEmail!.isEmpty) return "Email obligatorio";
+
+  String patron = r'\w+@\w+\.\w+';
+  RegExp regex = RegExp(patron);
+
+  if (!regex.hasMatch(formEmail)) return "Email invalido";
+
+  return null;
+}
+
+///Validacion de contraseña segura
+/// alt + 126
+String? validatePassword(String? formPassword) {
+  if (formPassword!.isEmpty) return "Contraseña es requerida";
+
+  String patron =
+      r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$';
+  RegExp regex = RegExp(patron);
+
+  if (!regex.hasMatch(formPassword)) {
+    return "Contraseña debe contener al menos 8 caracteres, incluidos mayusculas, minusculas, numeros y simnbolos";
   }
 
-  ///Validacion de formato email
-  String? validateEmail(String? formEmail) {
-    if (formEmail!.isEmpty) return "Email obligatorio";
+  return null;
+}
 
-    String patron = r'\w+@\w+\.\w+';
-    RegExp regex = RegExp(patron);
+///Muestra mensajes toast
+void showToast(String message) {
+  Toast.show(message, duration: Toast.lengthLong, gravity: Toast.bottom);
+}
 
-    if (!regex.hasMatch(formEmail)) return "Email invalido";
-
-    return null;
-  }
-
-  ///Validacion de contraseña segura
-  /// alt + 126
-  String? validatePassword(String? formPassword) {
-    if (formPassword!.isEmpty) return "Contraseña es requerida";
-
-    String patron =
-        r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$';
-    RegExp regex = RegExp(patron);
-
-    if (!regex.hasMatch(formPassword)) {
-      return "Contraseña debe contener al menos 8 caracteres, incluidos mayusculas, minusculas, numeros y simnbolos";
-    }
-
-    return null;
-  }
+/// Muestra dialogo de confirmación
+void showDialogMessage(BuildContext context,
+    {title,
+    titleActionOne,
+    titleActionTwo,
+    Function? callbackYes,
+    Function? callbackNo}) {
+  showPlatformDialog(
+      context: context,
+      builder: (_) => BasicDialogAlert(
+            title: Text(isNull(title) ? title : 'Mensaje',
+                style: const TextStyle(fontSize: 20),
+                textAlign: TextAlign.justify),
+            actions: [
+              BasicDialogAction(
+                title: Text(isNull(titleActionOne) ? titleActionOne : 'Sí'),
+                onPressed: () {
+                  if (isNull(callbackYes)) {
+                    callbackYes!(context);
+                  } else {
+                    showToast('Aceptado');
+                  }
+                  Navigator.pop(context);
+                },
+              ),
+              BasicDialogAction(
+                title: Text(isNull(titleActionTwo) ? titleActionTwo : 'No'),
+                onPressed: () {
+                  if (isNull(callbackNo)) {
+                    callbackNo!(context);
+                  }
+                  Navigator.pop(context);
+                },
+              )
+            ],
+          ));
+}
