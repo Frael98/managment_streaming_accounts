318a9c8fa7421c03849d682199458b06eb673bc3
Frael98 <frael1984@gmail.com>
2024-05-18 23:54:35 -0500

    view plataforma

--- lib/forms/plataforma/platform_list.dart
+++ lib/forms/plataforma/platform_list.dart
@@ -5,6 +5,11 @@ class PlatformListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Plataformas"),
+      ),
+      
+    );
   }
 }
