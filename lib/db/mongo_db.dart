f5863fc49b549dacbe23b4bc7cfa46a0764e4912
Frael98 <frael1984@gmail.com>
2024-05-22 21:14:32 -0500

    se agrego conexion a mongodb

--- lib/db/mongo_db.dart
+++ lib/db/mongo_db.dart
@@ -1,5 +1,4 @@
 import 'dart:developer';
-
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:mongo_dart/mongo_dart.dart';
 
@@ -7,26 +6,47 @@ Future loadDotEnv() async {
   await dotenv.load(fileName: ".env");
 }
 
-Future<Db> connectMongo() async {
-  await loadDotEnv();
+class MongoConnection {
+  
+  String? _host;
+  String? _port;
+  String? _dbName;
+  Db? _db;
 
-  log(dotenv.env['MONGO_URL']! + dotenv.env['MONGO_DB']!);
-  var db = Db(dotenv.env['MONGO_URL']! + dotenv.env['MONGO_DB']!);
-  return db;
-}
+  Future<Db?> getConnection() async {
+    if (_db == null) {
+      try {
+        _db = await Db.create(await _getConnectionString());
+      } catch (e) {
+        log("Error getConnection: $e");
+      }
+    }
+    return _db;
+  }
 
-Future<void> consultarUsuario() async {
-  try {
-    var db = await connectMongo();
-    await db.open();
-    /* 
-    log('message');
-    var userCollection = db.collection('user');
-    userCollection.find().forEach((element) {
-      log(element.toString());
-    }); */
-  } catch (e) {
-    log("Error $e");
-    log("Stack trace: ${e.toString()}");
+  Future<String> _getConnectionString() async {
+    await loadDotEnv();
+    _host = dotenv.env['MONGO_HOST']!;
+    _port = dotenv.env['MONGO_PORT']!;
+    _dbName = dotenv.env['MONGO_DB']!;
+    return "$_host:$_port/$_dbName";
+  }
+
+  void closeConnection() {
+    _db?.close();
+  }
+
+  static void main() async {
+    final db = await MongoConnection().getConnection();
+
+    try {
+      await db!.open();
+      var userCollection = db.collection('user');
+      await userCollection.find().forEach((element) {
+        log(element.toString());
+      });
+    } catch (e) {
+      log("Error main: $e");
+    }
   }
 }
