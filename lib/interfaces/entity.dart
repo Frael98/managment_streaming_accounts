37be86cdae509261d42b6e9155cf276070f263bb
Frael98 <frael1984@gmail.com>
2024-06-01 22:55:55 -0500

    agregacion de metodos toMap

--- lib/interfaces/entity.dart
+++ lib/interfaces/entity.dart
@@ -1,8 +1,26 @@
+import 'package:mongo_dart/mongo_dart.dart';
+
+/// Entidad - clase con atributos generales para los modelos
 abstract class Entity {
+  /// Id para MongoDB
+  ObjectId? uid;
+
+  /// Id para SQLite
+  int? id;
   String? state;
   DateTime? createdAt;
   DateTime? updatedAt;
   DateTime? deletedAt;
 
-  Entity({this.state, this.createdAt, this.updatedAt, this.deletedAt});
+  Entity(
+      {this.uid,
+      this.id,
+      this.state,
+      this.createdAt,
+      this.updatedAt,
+      this.deletedAt});
+  Map<String, dynamic> toMap();
+
+  /// Metodo creado para inserción de datos tipo date a string
+  Map<String, dynamic> toMapForSQLite();
 }
