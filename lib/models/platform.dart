798ab3c4a06acd3cb0d5bdf633158a481a9743e1
Frael98 <frael1984@gmail.com>
2024-06-01 20:07:32 -0500

    correciones y refactorizacion, renombramiento de modelo subscription

--- lib/models/platform.dart
+++ lib/models/platform.dart
@@ -7,26 +7,43 @@
   HBO
  */
 import 'package:f_managment_stream_accounts/interfaces/entity.dart';
+import 'package:mongo_dart/mongo_dart.dart';
 
 class Platform extends Entity {
-  
-  int? idPlatform;
-  String nameService;
+  String? namePlatform;
+  String? description;
 
-  Platform({this.idPlatform, required this.nameService});
+  Platform(
+      {ObjectId? uid, int? id, required this.namePlatform, this.description})
+      : super(uid: uid, id: id);
+  Platform.id({id});
 
+  @override
   Map<String, dynamic> toMap() {
-    // alias: toJson
     return {
-      'idPlatform': idPlatform,
-      'nameService': nameService,
+      'name_platform': namePlatform,
+      'description': description,
     };
   }
 
+  @override
   factory Platform.fromMap(Map<String, dynamic> map) {
     return Platform(
-        idPlatform: map['idPlatform'],
-        nameService: map['nameService'],
-        );
+      uid: map['_id'],
+      id: map['id_platform'] ?? map['ID_PLATFORM'],
+      namePlatform: map['name_platform'] ?? map['NAME_PLATFORM'],
+      description: map['description'] ?? map['DESCRIPTION'],
+    );
+  }
+
+  @override
+  String toString() {
+    return "uid(MongoId): $uid, id(SQLite): $id, namePlatform: $namePlatform, description: $description";
+  }
+  
+  @override
+  Map<String, dynamic> toMapForSQLite() {
+    // TODO: implement toMapForSQLite
+    throw UnimplementedError();
   }
 }
