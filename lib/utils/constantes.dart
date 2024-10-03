b8e09e39a888ca32b4c8553ec90f4a57a3af2724
Frael98 <frael1984@gmail.com>
2024-10-02 20:00:23 -0500

    clase Menu y correccion de gradientes

--- lib/utils/constantes.dart
+++ lib/utils/constantes.dart
@@ -1,24 +1,35 @@
+// ignore_for_file: constant_identifier_names
+
 import 'package:flutter/material.dart';
 
 const List<int> capacidad = [1, 2, 3, 4, 5];
 
 const colorsLinear = [
   LinearGradient(
-      colors: [Color.fromARGB(255, 211, 29, 16), Color.fromARGB(255, 221, 42, 42)],
-      begin: Alignment.topCenter,
-      end: Alignment.bottomCenter),
+      colors: [Color(0xFFBEC5FC), Color(0xFFE0C3FC)],
+      begin: Alignment.bottomLeft,
+      end: Alignment.topRight,
+      transform: GradientRotation(62.0)),
   LinearGradient(
-      colors: [Color.fromARGB(255, 20, 88, 143), Color.fromARGB(255, 70, 154, 223)],
-      begin: Alignment.topCenter,
-      end: Alignment.bottomCenter),
-  LinearGradient(
+      colors: [Color(0xFF85FFBD), Color(0xFFFFFB70)],
+      begin: Alignment.bottomLeft,
+      end: Alignment.topRight),
+  /* LinearGradient(
       colors: [Colors.purple, Color.fromARGB(255, 202, 106, 231)],
       begin: Alignment.topCenter,
-      end: Alignment.bottomCenter),
+      end: Alignment.bottomCenter), */
   LinearGradient(
-      colors: [Color.fromARGB(255, 46, 128, 49), Color.fromARGB(255, 103, 192, 44)],
+      colors: [Color(0xFFFBAB7E), Color(0xFFF7CE68)],
+      begin: Alignment.bottomLeft,
+      end: Alignment.topRight),
+  LinearGradient(
+      colors: [Color(0xFF8BC6EC), Color(0xFF9599E2)],
       begin: Alignment.topCenter,
       end: Alignment.bottomCenter),
+  LinearGradient(
+      colors: [Color(0xFFF4D03D), Color(0xFF16A085)],
+      begin: Alignment.topLeft,
+      end: Alignment.bottomRight),
 ];
 
 /// Colores para los estados
@@ -30,13 +41,26 @@ const colorStates = {
 };
 
 enum StateAccount {
-    available('disponible'),
-    drop('caida'),
-    //agotada('agotada'),
-    partialAvailable('parcialmente disponible'),
-    busy('ocupada');
+  available('disponible'),
+  drop('caida'),
+  //agotada('agotada'),
+  partialAvailable('parcialmente disponible'),
+  busy('ocupada');
 
-    final String nombre;
+  final String nombre;
 
-    const StateAccount(this.nombre);
+  const StateAccount(this.nombre);
+}
+
+class Menu {
+  static const String LOG_OUT = 'Cerrar Sesión';
+  static const String CLIENTS = 'Clientes';
+  static const String ACCOUNTS = 'Cuentas';
+  static const String SUBSCRIPTIONS = 'Subscripciones';
+  static const String PLATFORMS = 'Plataformas';
+
+  final String nombre;
+  final String ruta;
+
+  Menu(this.nombre, this.ruta);
 }
